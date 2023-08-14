5b69526870c5e8bfef94d537dc0ae67d71377d33
Jonster5 <onlyaccelerating@gmail.com>
2023-08-13 21:47:27 -0400

    oh yup

--- src/npc.ts
+++ src/npc.ts
@@ -2,6 +2,8 @@ import { Component, ECS, Vec2 } from 'raxis';
 import { Transform } from 'raxis-plugins';
 import crypto from 'crypto';
 import { Health } from './health';
+import { PassiveAI } from './ai';
+import { getServerPath } from 'raxis-server';
 
 export type NPCType = 'pig' | 'chicken';
 
@@ -14,9 +16,13 @@ export class NPC extends Component {
 }
 
 export function generateNPCs(ecs: ECS) {
-	ecs.spawn(new NPC('pig'), new Transform(new Vec2(100, 200)), new Health(100));
+	ecs.spawn(new PassiveAI(), new NPC('pig'), new Transform(new Vec2(100, 200)), new Health(100));
+}
+
+export function updateClients(ecs: ECS) {
+	getServerPath(ecs, 'game').server.clients.forEach((socket) => {});
 }
 
 export function NPCPlugin(ecs: ECS) {
-	ecs.addComponentType(NPC);
+	ecs.addComponentType(NPC).addStartupSystem(generateNPCs).addMainSystem(updateClients);
 }
